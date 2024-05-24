cb0edd11871e4dc84bd322bcc57fa53a12888ce4
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-24 09:01:41 +0200

    add foreign key to the projects table

--- database/migrations/2024_05_23_172000_add_type_id_foreign_key_to_projects_tabel.php
+++ database/migrations/2024_05_23_172000_add_type_id_foreign_key_to_projects_tabel.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('type_id')->after('description');
+            $table->unsignedBigInteger('type_id')->after('description')->nullable();
 
             $table->foreign('type_id')
             ->references('id')
