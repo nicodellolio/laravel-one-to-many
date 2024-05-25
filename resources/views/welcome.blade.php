bf9ea940265cef2a58ff46ccb4678b5d66ea8c64
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-25 13:24:49 +0200

    add style to the welcome page and layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,30 @@
 @extends('layouts.app')
 @section('content')
+    <div class="jumbotron p-bg-lighter rounded-3 mt-5 mx-5 d-flex justify-content-evenly">
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
-        <div class="logo_laravel">
+        <div class="left-side ps-3 z-2">
+
+            <h1 class="fw-bold">
+                Full-Stack Portfolio
+            </h1>
+
+
+            <p class="fs-4 w-50 fw-lighter">
+                Welcome to my Full-Stack Web Developer Portfolio. Here I collect some of my major projects created in these
+                years.
+                I developed my skills throughtout a full-course with <span class="fw-semibold"
+                    style="color: limegreen">Boolean</span>.
+            </p>
+
 
         </div>
-        <h1 class="display-1 fw-bold">
-            Full-Stack Portfolio 
-        </h1>
 
-        <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
-        <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
+        <img class="jumbo-img z-1" src="{{ asset('storage/' . 'uploads/jumbo.png') }}" alt="">
+
     </div>
-</div>
 
-<div class="content">
 
-</div>
+    <div class="content">
+
+    </div>
 @endsection
